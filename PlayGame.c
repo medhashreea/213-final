f716c370e075612dbd95c70f45262bdf54ac1d38
trappaly <trappaly@grinnell.edu>
2024-11-22 10:01:32 -0600

    starter functions

---
+++ PlayGame.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdbool.h>
+// #include <SDL2/SDL.h>
+
+
+/*
+* Creates grid
+*/
+void create_grid(int x, int y) {
+}
+
+/*
+* Creates player (initializes start position)
+*/
+void create_player(){
+}
+
+/*
+* Initialize state
+*/
+void initialize_state() {
+}
+
+/*
+* Update state 
+*/
+void update_state() {
+
+}
+
+/*
+* Update grid 
+*/
+void update_grid() {
+
+}
+
+/*
+* Checks if it's a snake 
+*/
+bool isSnake() {
+
+}
+
+/*
+* Check if it's a ladder
+*/
+
+bool isLadder() {
+
+}
+
+
+
+
+
+
+
+int main (int argc, char** argv) {
+
+}
